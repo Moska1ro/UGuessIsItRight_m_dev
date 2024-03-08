75f391607eb7083ef690baa9a26fbeb8dd6096ce
Euphocc <xu2954661568@gmail.com>
2024-03-08 23:37:17 +0800

    Frame.vue line77增加了测试用click。

--- src/pages/Frame.vue
+++ src/pages/Frame.vue
@@ -74,7 +74,7 @@
           prepend-icon="mdi-account-outline"
           title="function 6"
           :active="route.meta.name === 'users'"
-          @click="console.log(userInfo.isLoggedin) // 测试用的" 
+          @click="console.log(userInfo.isLoggedin)" 
         ></v-list-item>
       </v-list>
     </v-navigation-drawer>
